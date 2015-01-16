5f3a6514eef157686789437a43e30a620fbc3c16
jlegind <klegind@gmail.com>
2015-01-16 15:48:02 +0100

    nc

--- exeSearch.py
+++ exeSearch.py
@@ -8,9 +8,9 @@ from searchAPI import SearchAPI
 # my_api.take_parameters("locationId", "locality", "country", "status",
 #                        "establishmentMeans", "sourceTaxonKey")
 
-my_api = SearchAPI('http://api.gbif.org/v1/species/match?', 'G:/test/taxon.txt', 'G:/test/GISDnubtaxonomy_test.txt')
-my_api.take_parameters("usageKey", "species", "scientificName", "canonicalName", "rank",
-name=3, kingdom=4)
+#my_api = SearchAPI('http://api.gbif.org/v1/species/match?', 'G:/test/taxon.txt', 'G:/test/GISDnubtaxonomy_test.txt')
+#my_api.take_parameters("usageKey", "species", "scientificName", "canonicalName", "rank",
+#name=3, kingdom=4)
 
 # my_api = SearchAPI('http://api.gbif.org/v1/species/', 'G:/test/GISDnubtaxonomy_unique.txt', 'G:/test/GISDnubtaxonomy_full_test.txt')
 # my_api.take_parameters("key", "nubKey", "taxonID", "kingdom", "phylum", "order", "family", "genus",
@@ -25,8 +25,8 @@ name=3, kingdom=4)
 #                    'G:/test/no_param_test2.txt')
 # my_api.take_parameters("order", "family", "genus", "species")
 
-# my_api = SearchAPI('http://api.gbif.org/v1/occurrence/search?scientificName=', 'G:/test/occ.txt', 'G:/test/occurrence_test.txt')
-# my_api.take_parameters("order", "species", "country")
+my_api = SearchAPI('http://api.gbif.org/v1/species/match?kingdom=plantae&name=', 'G:/Custom exports/IUCN/species_search.txt', 'G:/test/IUCN_tax.txt')
+my_api.take_parameters("usageKey", "species", "scientificName", "canonicalName", "rank")
 
 # api = SearchAPI('http://api.gbif.org/v1/species/', 'G:/test/GISDnubtaxonomy_unique.txt',
 #                 'G:/test/vernacular_mk2.txt', suffix="/vernacularNames")
@@ -34,3 +34,14 @@ name=3, kingdom=4)
 #
 # api = SearchAPI('http://api.gbif.org/v1/occurrence/download/dataset/', 'G:/test/downloads.txt', 'G:/test/download_stats.txt')
 # api.take_parameters('numberRecords', 'download')
+
+#my_api = SearchAPI('http://api.gbif.org/v1/species/match?name=', 'G:/sweden_invasives/species.txt', 'G:/sweden_invasives/invasives_taxonomy.txt')
+#my_api.take_parameters("usageKey", "kingdom", "family", "scientificName")
+
+#my_api = SearchAPI('http://api.gbif.org/v1/organization/', 'G:/To publishers/arthro.txt', 'G:/To publishers/arthro_out.txt')
+#my_api.take_parameters("title")
+#ALA API call --------------------
+# my_api = SearchAPI('http://collections.ala.org.au/ws/dataResource/', 'G:/ALA/api.txt', 'G:/ALA/lic.txt')
+# my_api.take_parameters("name", "licenseType")
+#---------------------------------
+
